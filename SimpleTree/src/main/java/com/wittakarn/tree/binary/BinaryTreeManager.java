7df46c60b44174f6d7e007a43377b6b1af7f4673
Wittakarn <Wittakarn@x-laptop>
2014-02-05 22:03:12 +0700

    Create BinaryTreeManager class.

---
+++ SimpleTree/src/main/java/com/wittakarn/tree/binary/BinaryTreeManager.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wittakarn.tree.binary;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Wittakarn
+ */
+public class BinaryTreeManager implements Serializable {
+    
+    private BinaryTreeNode root;
+
+    public void add(Object newNode) {
+        BinaryTreeNode start = null, pre = null,tmp = null;
+        
+        try {
+            if (newNode != null) {
+                start = root;
+            }
+        } finally {
+
+        }
+    }
+}
